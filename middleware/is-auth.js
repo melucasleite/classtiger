48c85849bfaea199218d4794b2f8b7d4b89d53c0
Lucas Leite <me@lucasleite.us>
2019-06-13 08:15:50 -0300

    Fix token wasn't checking for signature
    Vulnerability
    Server would accept any token regardless of the secret.
    
    Root Cause
    jwt.decode only decodes without checking signature against the secret
    jwt.verify is the proper method
    
    Damage
    Vulnerability discovered while writing unit tests before product release

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -1,22 +1,27 @@
 const jwt = require("jsonwebtoken");
 const APP_SECRET = process.env.APP_SECRET || "not_a_secret";
-
 function isAuth(req, res, next) {
-  const authHeader = req.get("Authorization");
-  if (!authHeader) {
-    const error = new Error("Missing Authorization Header.");
-    error.statusCode = 401;
-    throw error;
+  try {
+    const authHeader = req.get("Authorization");
+    if (!authHeader) {
+      const error = new Error("Missing Authorization Header.");
+      error.statusCode = 401;
+      throw error;
+    }
+    const token = authHeader.split(" ")[1];
+    const decodedToken = jwt.verify(token, APP_SECRET);
+    if (!decodedToken) {
+      const error = new Error("Not Authenticated.");
+      error.statusCode = 401;
+      throw error;
+    }
+    req.userId = decodedToken.userId;
+    next();
+    return decodedToken;
+  } catch (error) {
+    next(error);
+    return error;
   }
-  const token = authHeader.split(" ")[1];
-  const decodedToken = jwt.decode(token, APP_SECRET);
-  if (!decodedToken) {
-    const error = new Error("Not Authenticated.");
-    error.statusCode = 401;
-    throw error;
-  }
-  req.userId = decodedToken.userId;
-  next();
 }
 
 module.exports = isAuth;
